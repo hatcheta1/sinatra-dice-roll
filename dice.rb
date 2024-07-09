5381ba112a559aeac4f89ebf5a5367acd1c2e7b8
Ashanti Hatchett <ahatchett02@gmail.com>
2024-07-09 03:14:11 +0000

    created layout for view templates

--- dice.rb
+++ dice.rb
@@ -9,7 +9,7 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  erb(:elephant)       
+  erb(:elephant, {:layout => :wrapper})       
 end
 
 get("/zebra") do
@@ -27,7 +27,7 @@ get("/dice/2/6") do
 
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  erb(:two_six)
+  erb(:two_six, {:layout => :wrapper})
 end
 
 get("/dice/2/10") do
@@ -37,7 +37,7 @@ get("/dice/2/10") do
 
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-    erb(:two_ten)
+    erb(:two_ten, {:layout => :wrapper})
 end
 
 get("/dice/1/20") do
@@ -45,7 +45,7 @@ get("/dice/1/20") do
 
   @outcome = "You rolled a #{first_die}."
 
-  erb(:one_twenty)
+  erb(:one_twenty, {:layout => :wrapper})
 end
 
 get("/dice/5/4") do
@@ -59,5 +59,5 @@ get("/dice/5/4") do
 
   @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a sum of #{sum}."
 
-  erb(:five_four)
+  erb(:five_four, {:layout => :wrapper})
 end
